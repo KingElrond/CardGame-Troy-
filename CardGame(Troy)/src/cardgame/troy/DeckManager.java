69b2a792e5c5aaa83bc3a3eb684ffc5163c36fc0
KingElrond <66854137+KingElrond@users.noreply.github.com>
2021-02-09 11:24:56 -0800

    More stuff now debugger with breakpoints doesn't work for some reason

--- CardGame(Troy)/src/cardgame/troy/DeckManager.java
+++ CardGame(Troy)/src/cardgame/troy/DeckManager.java
@@ -14,8 +14,8 @@ import java.util.Arrays;
  */
 public class DeckManager {
 public ArrayList<String> unshuffledDeck;
-public ArrayList<Integer> ShuffledDeck;
-public ArrayList<Integer> calledCards;
+public ArrayList<Integer> ShuffledDeck = new ArrayList();
+public ArrayList<Integer> calledCards = new ArrayList();
 public DeckManager()
  {
         unshuffledDeck = new ArrayList<>(Arrays.asList("","Ace of Spades","10 of Spades","9 of Spades","8 of Spades","7 of Spades","6 of Spades","5 of Spades","4 of Spades","3 of Spades","2 of Spades","King of Spades","Queen of Spades","Jack of Spades",""
@@ -25,24 +25,33 @@ public DeckManager()
         
      
  }
-  
+public void shuffleInit()
+{
+    shuffleDeck();
+}
+
 private void shuffleDeck()
 {
     boolean isrepeat=false;
-    int randNum=0;
+    int randNum = (int) (Math.random() * (52 - 1)) + 1;;
     for(int i=0; i<unshuffledDeck.size(); i++)
     {
-        isrepeat=false;
+        isrepeat=true;
         while(isrepeat)
         {
+            boolean stupidBool = false;
         randNum = (int) (Math.random() * (52 - 1)) + 1;
         for(int g : ShuffledDeck)
         {
             if(g==randNum)
             {
                 isrepeat=true;
+                stupidBool=true;
             }
-            
+            if(stupidBool==false)
+            {
+                isrepeat=false;
+            }
         }
         }
     ShuffledDeck.add(randNum);
@@ -52,8 +61,8 @@ private void shuffleDeck()
 
 public String drawCard()
 {
-    int cardtypenum=ShuffledDeck.get(0);
-    ShuffledDeck.remove(0);
+    int cardtypenum=ShuffledDeck.get(1);
+    ShuffledDeck.remove(1);
     String cardtoreturn = unshuffledDeck.get(cardtypenum);
   return cardtoreturn;
 }
