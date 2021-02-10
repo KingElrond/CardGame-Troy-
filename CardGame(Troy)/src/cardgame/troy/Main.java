1cbc15396bb8b64a20cf49852ee211913a73138e
KingElrond <66854137+KingElrond@users.noreply.github.com>
2021-02-10 10:40:20 -0800

    did some stuff

--- CardGame(Troy)/src/cardgame/troy/Main.java
+++ CardGame(Troy)/src/cardgame/troy/Main.java
@@ -15,19 +15,29 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+String[] hand1;
+hand1 = new String[5];
         // TODO code application logic here
         DeckManager dm = new DeckManager();
         
-        for(int i = 0; i<10; i++)
-        {
         dm.shuffleInit();
+        for(int i=0; i<5; i++)
+        {
+        hand1[i]=dm.drawCard();
+        
+        }
+        for(String card : hand1)
+        {
+        	System.out.println(card);
+        }
+        /*
         System.out.println(dm.drawCard());
         System.out.println(dm.ShuffledDeck);
         for(int g: dm.ShuffledDeck)
         {
         	System.out.println(dm.unshuffledDeck.get(g));
         }
-        }
+        */
     }
     
 }
